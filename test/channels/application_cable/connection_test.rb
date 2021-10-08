a02b65c152a2de45c2d4834ce6e6dab85c8336b8
yana-gi <yanagisawa.hika@gmail.com>
2021-10-08 17:29:40 +0900

    rubocop修正適用

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,4 +1,7 @@
-require "test_helper"
+# frozen_string_literal: true
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
