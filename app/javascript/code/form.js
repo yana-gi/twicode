b7f0055be5551c20b61cf729228462772bd586d9
yana-gi <yanagisawa.hika@gmail.com>
2021-10-12 12:58:00 +0900

    言語を選択できるようにした

--- app/javascript/code/form.js
+++ app/javascript/code/form.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     hljs.highlightAll();
     let textarea = document.querySelector("#code_body");
     let precode = document.querySelector("pre code");
+    let language = document.querySelector("#parse_language");
 
     textarea.addEventListener('input', highlight);
+    language.addEventListener('change', highlight);
 
     function highlight() {
-        precode.innerHTML = hljs.highlight(textarea.value, {language: 'ruby'}).value
+        precode.className = language.value
+        precode.innerHTML = hljs.highlight(textarea.value, {language: language.value}).value
         hljs.highlightAll();
     }
 });
