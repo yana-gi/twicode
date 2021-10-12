a871eef407643e09b8732b32bef7a00e55f2a76f
yana-gi <yanagisawa.hika@gmail.com>
2021-10-12 16:35:40 +0900

    addEventListener のイベントをDOMContentLoadedからturbolinks:loadに変更
    DOMContentLoadedの場合、画面遷移時に発火しないため

--- app/javascript/code/form.js
+++ app/javascript/code/form.js
@@ -1,10 +1,11 @@
 const hljs = require('highlight.js/lib/common');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    hljs.highlightAll();
+document.addEventListener('turbolinks:load', (event) => {
     let textarea = document.querySelector("#code_body");
     let precode = document.querySelector("pre code");
     let language = document.querySelector("#parse_language");
+
+    precode.className = "ruby hljs language-ruby"
 
     textarea.addEventListener('input', highlight);
     language.addEventListener('change', highlight);
