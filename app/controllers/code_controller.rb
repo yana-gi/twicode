96899369be9793e39b7e9c9afa5e0bf986399754
yana-gi <yanagisawa.hika@gmail.com>
2021-10-12 15:06:49 +0900

    不要なコメント削除

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -3,20 +3,16 @@
 class CodeController < ApplicationController
   before_action :set_code, only: %i[show update destroy]
 
-  # GET /code or /code.json
   def index
     @code = Code.all
   end
 
-  # GET /code/1 or /code/1.json
   def show; end
 
-  # GET /code/new
   def new
     @code = Code.new
   end
 
-  # POST /code or /code.json
   def create
     @code = Code.new(code_params)
 
@@ -31,7 +27,6 @@ class CodeController < ApplicationController
     end
   end
 
-  # PATCH/PUT /code/1 or /code/1.json
   def update
     respond_to do |format|
       if @code.update(code_params)
@@ -44,7 +39,6 @@ class CodeController < ApplicationController
     end
   end
 
-  # DELETE /code/1 or /code/1.json
   def destroy
     @code.destroy
     respond_to do |format|
@@ -55,12 +49,10 @@ class CodeController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_code
     @code = Code.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def code_params
     params.require(:code).permit(:body)
   end
