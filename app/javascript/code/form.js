289092bf8e56c7c863220ae60768303b5d4deef3
yana-gi <yanagisawa.hika@gmail.com>
2021-10-18 17:15:04 +0900

    data_urlをparamsで受け取れるようにした（取得処理を入力毎に実行している状態）

--- app/javascript/code/form.js
+++ app/javascript/code/form.js
@@ -5,6 +5,7 @@ document.addEventListener('turbolinks:load', (event) => {
     let textarea = document.querySelector("#code_body");
     let precode = document.querySelector("pre code");
     let language = document.querySelector("#parse_language");
+    let hidden_image_url = document.querySelector('#code_image_url')
 
     precode.className = "ruby hljs language-ruby"
 
@@ -15,5 +16,13 @@ document.addEventListener('turbolinks:load', (event) => {
         precode.className = language.value
         precode.innerHTML = hljs.highlight(textarea.value, {language: language.value}).value
         hljs.highlightAll();
+        getDataUrl(event).then((dataUrl) => {
+            hidden_image_url.setAttribute('value', dataUrl);
+        })
+    }
+
+    async function getDataUrl(event) {
+        const canvas = await html2canvas(precode,{windowWidth:600, windowHeight:314})
+        return canvas.toDataURL('image/jpeg')
     }
 });
