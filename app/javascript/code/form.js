39a7ffd1e7e77d93c415ef80ea196658adc2962d
yana-gi <yanagisawa.hika@gmail.com>
2021-10-18 17:54:39 +0900

    formが存在する画面でのみ処理を実行させる

--- app/javascript/code/form.js
+++ app/javascript/code/form.js
@@ -2,31 +2,34 @@ const hljs = require('highlight.js/lib/common');
 import html2canvas from 'html2canvas';
 
 document.addEventListener('turbolinks:load', (event) => {
-    let textarea = document.querySelector("#code_body");
-    let precode = document.querySelector("pre code");
-    let language = document.querySelector("#parse_language");
-    let hidden_image_url = document.querySelector('#code_image_url')
 
-    precode.className = "ruby hljs language-ruby"
+    const form = document.querySelector('form');
+    const textarea = document.querySelector("#code_body");
+    const precode = document.querySelector("pre code");
+    const language = document.querySelector("#parse_language");
+    const hidden_image_url = document.querySelector('#code_image_url')
 
-    textarea.addEventListener('input', highlight);
-    language.addEventListener('change', highlight);
+    if (form) {
+        precode.className = "ruby hljs language-ruby"
+
+        textarea.addEventListener('input', highlight);
+        language.addEventListener('change', highlight);
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            getDataUrl().then((dataUrl) => {
+                hidden_image_url.setAttribute('value', dataUrl);
+            }).then(() => {
+                form.submit();
+            })
+        });
+    }
 
     function highlight() {
         precode.className = language.value
         precode.innerHTML = hljs.highlight(textarea.value, {language: language.value}).value
         hljs.highlightAll();
     }
-
-    const form = document.querySelector('form');
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        getDataUrl().then((dataUrl) => {
-            hidden_image_url.setAttribute('value', dataUrl);
-        }).then(() => {
-            form.submit();
-        })
-    });
 
     async function getDataUrl() {
         const canvas = await html2canvas(precode,{windowWidth:600, windowHeight:314})
