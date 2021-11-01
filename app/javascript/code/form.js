3945fb93209a024e3ea2542013ed06314a11ab5f
yana-gi <yanagisawa.hika@gmail.com>
2021-11-01 15:40:27 +0900

    eslint, prettier修正

--- app/javascript/code/form.js
+++ app/javascript/code/form.js
@@ -1,41 +1,47 @@
-const hljs = require('highlight.js/lib/common');
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
+const hljs = require("highlight.js/lib/common");
 
-document.addEventListener('turbolinks:load', (event) => {
+document.addEventListener("turbolinks:load", () => {
+  const form = document.querySelector("form");
+  const textarea = document.querySelector("#code_body");
+  const precode = document.querySelector("pre code");
+  const language = document.querySelector("#parse_language");
+  const hiddenImageDataUrl = document.querySelector("#code_image_data_url");
 
-    const form = document.querySelector('form');
-    const textarea = document.querySelector("#code_body");
-    const precode = document.querySelector("pre code");
-    const language = document.querySelector("#parse_language");
-    const hiddenImageDataUrl = document.querySelector('#code_image_data_url');
+  if (precode) {
+    hljs.highlightAll();
+  }
+  if (form) {
+    precode.className = "ruby hljs language-ruby";
 
-    if (precode) {
-        hljs.highlightAll();
-    }
-    if (form) {
-        precode.className = "ruby hljs language-ruby"
+    textarea.addEventListener("input", highlight);
+    language.addEventListener("change", highlight);
 
-        textarea.addEventListener('input', highlight);
-        language.addEventListener('change', highlight);
-
-        form.addEventListener("submit", function (event) {
-            event.preventDefault();
-            getImageDataUrl().then((imageDataUrl) => {
-                hiddenImageDataUrl.setAttribute('value', imageDataUrl);
-            }).then(() => {
-                form.submit();
-            })
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      getImageDataUrl()
+        .then((imageDataUrl) => {
+          hiddenImageDataUrl.setAttribute("value", imageDataUrl);
+        })
+        .then(() => {
+          form.submit();
         });
-    }
+    });
+  }
 
-    function highlight() {
-        precode.className = language.value
-        precode.innerHTML = hljs.highlight(textarea.value, {language: language.value}).value
-        hljs.highlightAll();
-    }
+  function highlight() {
+    precode.className = language.value;
+    precode.innerHTML = hljs.highlight(textarea.value, {
+      language: language.value,
+    }).value;
+    hljs.highlightAll();
+  }
 
-    async function getImageDataUrl() {
-        const canvas = await html2canvas(precode,{windowWidth:600, windowHeight:314})
-        return canvas.toDataURL('image/jpeg', 1.0)
-    }
+  async function getImageDataUrl() {
+    const canvas = await html2canvas(precode, {
+      windowWidth: 600,
+      windowHeight: 314,
+    });
+    return canvas.toDataURL("image/jpeg", 1.0);
+  }
 });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-require('../code/form')
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
 import "../src/application.scss";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("../code/form");
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
