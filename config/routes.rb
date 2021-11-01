c7fc8a422f0a343f7fa52e650d2c83d9c8c7a3bb
yana-gi <yanagisawa.hika@gmail.com>
2021-11-01 16:02:45 +0900

    tracerouteが通るようにルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'code#index'
-  resources :code
+  resources :code, only: [:index, :show, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
