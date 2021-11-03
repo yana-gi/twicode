5b6a94d28db3dccc6b6fcef08adda8a1731c1069
yana-gi <yanagisawa.hika@gmail.com>
2021-11-03 16:57:37 +0900

    code一覧画面をユーザー別に表示するようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   root 'welcome#index'
-  get 'welcome', to: 'welcome#index', as: 'welcome'
-  resources :code, only: [:index, :show, :new, :create, :destroy]
+  resources :code, only: [:show, :new, :create, :destroy]
+  get 'users/:user_id/code', to: 'code#index', as: 'user_code_index'
 
+  get 'welcome', to: 'welcome#index', as: 'welcome'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 end
