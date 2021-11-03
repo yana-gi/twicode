d2679d2621fe7d96fe97ea17ff64b19cc2e7b972
yana-gi <yanagisawa.hika@gmail.com>
2021-11-03 13:01:12 +0900

    ログイン/ログアウト処理作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,29 @@
 
 class SessionsController < ApplicationController
   def create
-    user_data = request.env['omniauth.auth']
-    session[:nickname] = user_data[:info][:nickname]
-    redirect_to root_path, notice: 'ログインしました'
+    user = User.find_or_create_from_auth_hash(auth_params)
+    if user
+      log_in(user)
+      flash[:notice] = 'ログインしました'
+    else
+      flash[:notice] = '失敗しました'
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    log_out if logged_in?
+    flash[:notice] = 'ログアウトしました'
+    redirect_to root_path
+  end
+
+  def failure
+    flash[:notice] = 'キャンセルしました'
+    redirect_to root_path
+  end
+
+  private
+  def auth_params
+    request.env['omniauth.auth']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth_hash(auth_hash)
+    uid = auth_hash[:uid]
+    twitter_id = auth_hash[:info][:nickname]
+    twitter_name = auth_hash[:info][:name]
+    twitter_icon = auth_hash[:info][:image]
+
+    find_or_create_by(uid: uid) do |user|
+      user.uid = uid
+      user.twitter_id = twitter_id
+      user.twitter_name = twitter_name
+      user.twitter_icon = twitter_icon
+    end
+  end
 end
