4455100b22a261026dcf76e30cd505197b6bf077
yana-gi <yanagisawa.hika@gmail.com>
2021-11-03 11:36:37 +0900

    userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2021_11_08_024536) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "language", default: 0, null: false
     t.bigint "user_id", null: false
+    t.integer "language", default: 0, null: false
     t.index ["user_id"], name: "index_code_on_user_id"
   end
 
