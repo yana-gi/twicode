a89f4a0e40ed04bb7cc09c6d5f005ab864241775
yana-gi <yanagisawa.hika@gmail.com>
2021-11-05 14:46:44 +0900

    Userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.find_or_create_from_auth_hash' do
+    let(:auth_hash) do
+      {
+        provider: 'twitter',
+        uid: 'uid',
+        info: {
+          nickname: 'twicode',
+          name: 'ついこーど',
+          image: 'http://example.com/twicode.jpg'
+        }
+      }
+    end
+
+    context 'uidに対応するUserが作成されていないとき' do
+      it '引数で設定した属性のUserオブジェクトが返ること' do
+        user = User.find_or_create_from_auth_hash(auth_hash)
+        expect(user.uid).to eq 'uid'
+        expect(user.twitter_id).to eq 'twicode'
+        expect(user.twitter_name).to eq 'ついこーど'
+        expect(user.twitter_icon).to eq 'http://example.com/twicode.jpg'
+        expect(user).to be_persisted
+      end
+
+      it 'Userモデルのレコードが一件増えていること' do
+        expect { User.find_or_create_from_auth_hash(auth_hash) }.to change { User.count }.from(0).to(1)
+      end
+    end
+
+    context 'uidに対応するUserが既に作成されているとき' do
+      let!(:created_user) { FactoryBot.create(:user, uid: 'uid') }
+
+      it '引数に対応するUserレコードのオブジェクトが返ること' do
+        user = User.find_or_create_from_auth_hash(auth_hash)
+        expect(user).to eq created_user
+      end
+
+      it 'Userモデルのレコード件数が変化していないこと' do
+        expect { User.find_or_create_from_auth_hash(auth_hash) }.not_to change(User.count)
+      end
+    end
+  end
 end
