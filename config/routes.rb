7de258b5a735abaeb6bc9a0b751b18084b9a5d37
yana-gi <yanagisawa.hika@gmail.com>
2021-11-07 16:12:05 +0900

    TOPページを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'code#index'
+  get 'welcome/index'
+  root 'welcome#index'
+  get 'welcome', to: 'welcome#index', as: 'welcome'
   resources :code, only: [:index, :show, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
