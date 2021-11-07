01e5e3bd9d0d65b4c1466919992c37f51e4f8696
yana-gi <yanagisawa.hika@gmail.com>
2021-11-07 16:58:38 +0900

    ログイン状態じゃないとcode編集アクション(new/create/destroy)ができないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to welcome_url unless current_user
+  end
 end
