2c74d768e1f96c32f6aa2091ebcde848faf564ec
yana-gi <yanagisawa.hika@gmail.com>
2021-11-08 12:46:47 +0900

    Codeにlanguage属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_055147) do
+ActiveRecord::Schema.define(version: 2021_11_08_024536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,12 +47,7 @@ ActiveRecord::Schema.define(version: 2021_10_18_055147) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "codes", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "language", default: 0, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
