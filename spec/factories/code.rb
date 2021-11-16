c7a70ba147889178b0e44693193fb476c49fe259
yana-gi <yanagisawa.hika@gmail.com>
2021-11-16 17:59:35 +0900

    codeのFactoryBotに画像を紐付けるようにした

--- spec/factories/code.rb
+++ spec/factories/code.rb
@@ -3,5 +3,16 @@
 FactoryBot.define do
   factory :code do
     body { 'test' }
+    language { 'Ruby' }
+    user
+    after(:build) do |code|
+      code.attach_blob(image_data_url)
+    end
   end
+end
+
+# code画像のdata_url。
+# 本来の処理ではJS側でdata_urlを作成する
+def image_data_url
+  "data:image/png;base64,#{Base64.strict_encode64(File.open('spec/factories/test_code_image.png').read)}"
 end
