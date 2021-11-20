37e8fddeb269176fa2e0ca6dededb457d7e94833
yana-gi <yanagisawa.hika@gmail.com>
2021-11-20 15:34:49 +0900

    利用規約・プライバシーポリシーのリンクを作成

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,8 @@ class WelcomeController < ApplicationController
   skip_before_action :login_required
 
   def index; end
+
+  def tos; end
+
+  def privacy_policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
   resources :code, only: [:show, :new, :create, :destroy]
   get 'users/:user_id/code', to: 'code#index', as: 'user_code_index'
 
   get 'welcome', to: 'welcome#index', as: 'welcome'
+  get 'tos', to: 'welcome#tos', as: 'tos'
+  get 'privacy_policy', to: 'welcome#privacy_policy', as: 'privacy_policy'
+
   get '/auth/:provider/callback', to: 'sessions#create'
   get "/auth/failure", to: "sessions#failure"
   get '/logout', to: 'sessions#destroy'
