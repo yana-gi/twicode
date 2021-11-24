21a1ce9f9051a4355df05db71d565dd708a13193
yana-gi <yanagisawa.hika@gmail.com>
2021-11-24 16:35:02 +0900

    rubocop修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/LineLength
 languages = %w[Bash C C# C++ CSS Diff Go HTML XML JSON Java JavaScript Kotlin Markdown PHP PlainText Perl Python R Ruby Rust SCSS SQL Shell Swift TypeScript YAML]
+# rubocop:enable Layout/LineLength
 
 languages.each { |language| Language.create(name: language) }
