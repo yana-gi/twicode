6720c7879943372d84a3ae2f8b9d0d531925f211
yana-gi <yanagisawa.hika@gmail.com>
2021-12-07 21:18:02 +0900

    Action Cableを使用していなかったため外した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
-import "channels";
 
 require("../src/code");
 require("../src/header");
