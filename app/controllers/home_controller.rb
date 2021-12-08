9ef210847b6d4d1ae3e11e1242f37e6b3eb6f614
yana-gi <yanagisawa.hika@gmail.com>
2021-12-08 19:15:02 +0900

    code一覧をパーシャル化

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def top; end
+  def top
+    @code = current_user.code.order(:created_at).page params[:page]
+  end
 end
